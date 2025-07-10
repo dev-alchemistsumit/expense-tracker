dd44cfa9913558dbe937327d5fef968db2c706df
dev-alchemistsumit <thesumit0013@gmail.com>
2025-07-10 21:30:07 +0530

    Implementing Bonus Features

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,3 +9,5 @@ module.exports = {
 // This is a Tailwind CSS configuration file for a Remix application.
 // It specifies the paths to the content files where Tailwind CSS classes will be used,
 // extends the default theme, and includes no additional plugins.
+// The content paths ensure that Tailwind CSS purges unused styles in production builds.
+// The file is written in CommonJS format, which is compatible with Node.js environments.
